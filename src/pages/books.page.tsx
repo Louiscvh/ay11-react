4d60d1f1507ae39c58d8c80b0dd316778ddbe2b8
ryanscordino <hello@ryandanion.com>
2024-06-17 22:27:30 +0200

    update

---
+++ src/pages/books.page.tsx
@@ -0,0 +1,1 @@
+export const Books = () => {return <div>Books</div>}
---
+++ src/types/book.type.ts
@@ -0,0 +1,25 @@
+type BookData = {
+  id: number;
+  title: string;
+  resume: string;
+  author: string;
+  editor: string;
+  date: string;
+  language: string;
+  genre: string;
+  isbn: string;
+  review: {
+    avgNote: number;
+    reviewNumber: number;
+  };
+  cover: string;
+  library: {
+    name: string;
+    numberAvailable: number;
+    location: string;
+  }[];
+};
+
+type BooksData = {
+  books: BookData[];
+};
